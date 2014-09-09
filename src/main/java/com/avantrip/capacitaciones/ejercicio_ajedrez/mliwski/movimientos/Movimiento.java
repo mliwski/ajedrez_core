31a1f9d9e926b687a8ca18ba6e48b21885878bb2
mliwski <matias.liwski@gmail.com>
2014-09-09 00:42:22 -0300

    Se agregan tests al movimiento.
    Se tienen que agregar verificaciones de llamadas correcta, quizas verify
    de mockito?

--- src/main/java/com/avantrip/capacitaciones/ejercicio_ajedrez/mliwski/movimientos/Movimiento.java
+++ src/main/java/com/avantrip/capacitaciones/ejercicio_ajedrez/mliwski/movimientos/Movimiento.java
@@ -64,6 +64,37 @@ public class Movimiento {
 		MovimientoProcessor verticalProcessor = new MovimientoVerticalProcessor(diagonalProcessor);
 		return verticalProcessor;
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((destino == null) ? 0 : destino.hashCode());
+		result = prime * result + ((origen == null) ? 0 : origen.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Movimiento other = (Movimiento) obj;
+		if (destino == null) {
+			if (other.destino != null)
+				return false;
+		} else if (!destino.equals(other.destino))
+			return false;
+		if (origen == null) {
+			if (other.origen != null)
+				return false;
+		} else if (!origen.equals(other.origen))
+			return false;
+		return true;
+	}
 
 	@Override
 	public String toString() {
--- src/test/java/com/avantrip/capacitaciones/ejercicio_ajedrez/mliwski/commands/MoverCommandTest.java
+++ src/test/java/com/avantrip/capacitaciones/ejercicio_ajedrez/mliwski/commands/MoverCommandTest.java
@@ -1,11 +1,22 @@
 package com.avantrip.capacitaciones.ejercicio_ajedrez.mliwski.commands;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.mockito.Mockito.*;
+
+import java.util.Collections;
+import java.util.List;
 
 import org.junit.Test;
 
 import com.avantrip.capacitaciones.ejercicio_ajedrez.mliwski.Escaque;
+import com.avantrip.capacitaciones.ejercicio_ajedrez.mliwski.movimientos.Movimiento;
+import com.avantrip.capacitaciones.ejercicio_ajedrez.mliwski.notifications.Notification;
+import com.avantrip.capacitaciones.ejercicio_ajedrez.mliwski.preconditions.movimiento.MovimientoPrecondition;
+import com.avantrip.capacitaciones.ejercicio_ajedrez.mliwski.tableros.Tablero;
 import com.avantrip.capacitaciones.ejercicio_ajedrez.mliwski.tableros.TableroInstance;
+import com.avantrip.capacitaciones.ejercicio_ajedrez.mliwski.trebejos.Color;
+import com.avantrip.capacitaciones.ejercicio_ajedrez.mliwski.trebejos.Trebejo;
 
 public class MoverCommandTest {
 
@@ -41,4 +52,53 @@ public class MoverCommandTest {
 		new MoverCommand(tablero, origen, destino);
 	}
 
+	@Test
+	public void shouldBuildCommand() {
+		TableroInstance tablero = mock(TableroInstance.class);
+		Escaque origen = new Escaque('a',1);
+		Escaque destino = new Escaque('a',2);
+		Trebejo trebejo = mock(Trebejo.class);
+
+		when(tablero.getTrebejo(origen)).thenReturn(trebejo);
+		
+		new MoverCommand(tablero, origen, destino);
+	}
+
+	@Test
+	public void notificationsShouldBeEmpty() {
+		TableroInstance tablero = mock(TableroInstance.class);
+		Escaque origen = new Escaque('a',1);
+		Escaque destino = new Escaque('a',2);
+		Trebejo trebejo = spy(new TrebejoMock(Color.Blanco));
+
+		when(tablero.getTrebejo(origen)).thenReturn(trebejo);
+		when(tablero.getTrebejo(destino)).thenReturn(null);
+		
+		MoverCommand command = new MoverCommand(tablero, origen, destino);
+		List<Notification> notifications = command.ejecutar();
+		assertThat(notifications, is(empty()));
+	}
+	
+	private class TrebejoMock extends Trebejo {
+		private Trebejo trebejoCapturado = null;
+		
+		public TrebejoMock(Color color) {
+			super(color);
+		}
+
+		@Override
+		protected List<MovimientoPrecondition> getMovimientoPreconditions() {
+			return Collections.emptyList();
+		}
+		
+		@Override
+		public Trebejo getTrebejoCapturado(Tablero tablero, Movimiento movimiento) {
+			return this.trebejoCapturado;
+		}
+
+//		public void setTrebejoCapturado(Trebejo trebejoCapturado) {
+//			this.trebejoCapturado = trebejoCapturado;
+//		}
+		
+	}
 }
